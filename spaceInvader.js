988882e9b84170a050be3f55a277eec44faf172e
noham <noham.pace.alkhomaili@student.ehb.be>
2022-10-12 10:10:48 +0200

    taak 1 space invader

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,28 @@
 "use strict";
+window.onresize = spaceInvader;
+function spaceInvader(){
+    let canvas = document.querySelector("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    let context = canvas.getContext("2d");
 
-alert("Draw your space invader here");
+    context.beginPath();
+    context.rect(100, 100, 300, 300);
+    context.fill();
+
+    context.fillStyle = "#00ff00";
+    context.beginPath();
+    context.rect(225, 125, 50, 50);
+    context.rect(175, 175, 50, 50);
+    context.rect(275, 175, 50, 50);
+    context.rect(175, 325, 50, 50);
+    context.rect(175, 275, 150, 50);
+    context.rect(275, 325, 50, 50);
+
+
+    context.fill();
+
+    
+}
+spaceInvader();
+// alert("Draw your space invader here");
